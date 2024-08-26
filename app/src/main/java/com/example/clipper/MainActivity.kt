a7f62832d3ff0c046cc2280d27420e86eb5035b1
Misha Causur <mishacausur@mail.ru>
2024-08-26 23:55:31 +0500

    count w/ button

--- app/src/main/java/com/example/clipper/MainActivity.kt
+++ app/src/main/java/com/example/clipper/MainActivity.kt
@@ -7,11 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
@@ -38,13 +46,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var count by remember { mutableIntStateOf(0) }
+    Spacer(modifier = Modifier.height(66.dp))
     Row(modifier = Modifier.padding(44.dp)) {
+        Button(onClick = { count++ }, modifier = Modifier.width(88.dp)) { }
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(
-                text = "Hello $name!",
-                fontStyle = FontStyle(2),
-                modifier = modifier
-            )
+            Text(text = "Tune on $name")
+            Text(text = "wherever you go!")
+            Text(text = "score is $count")
         }
     }
 }
