afab42afcb19f852e372152a71f1c3e736ce8438
Misha Causur <mishacausur@mail.ru>
2024-08-26 23:10:09 +0500

    text modified

--- app/src/main/java/com/example/clipper/MainActivity.kt
+++ app/src/main/java/com/example/clipper/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.clipper.ui.theme.ClipperTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +26,8 @@ class MainActivity : ComponentActivity() {
             ClipperTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "there",
+                        modifier = Modifier.padding(innerPadding).background(Color.Cyan)
                     )
                 }
             }
@@ -32,10 +37,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row(modifier = Modifier.padding(44.dp)) {
+        Column {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
