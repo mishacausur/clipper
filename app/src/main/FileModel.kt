627ba19cfd96a46c3725484002a2c2e5175c7bcb
Misha Causur <mishacausur@mail.ru>
2024-08-29 21:29:25 +0500

    sealed interface added

--- app/src/main/FileModel.kt
+++ app/src/main/FileModel.kt
@@ -1,5 +1,12 @@
+import java.lang.Exception
+
 sealed interface FileModel {
     class VarFile: FileModel
     class MapFile: FileModel
     class TerFile: FileModel
+}
+
+sealed interface Result<out T: any> {
+    data class Success<T: Any>(val value: T): Result<T>
+    data class Error(val error: Exception): Result<Nothing>
 }
